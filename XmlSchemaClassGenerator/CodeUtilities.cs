38675ebc4e672697fe3a5aa0d44c820e57f78211
Mark Junker <m.junker@fubar-dev.de>
2014-12-18 14:28:46 +0100

    Owner is right, it's better to use IsLetter.

--- XmlSchemaClassGenerator/CodeUtilities.cs
+++ XmlSchemaClassGenerator/CodeUtilities.cs
@@ -7,15 +7,12 @@ namespace XmlSchemaClassGenerator
 {
     public static class CodeUtilities
     {
-        private const string Alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        //private const string Num = "0123456789";
-
         public static string ToNormalizedEnumName(this string name)
         {
             name = name.Trim().Replace(' ', '_').Replace('\t', '_');
             if (string.IsNullOrEmpty(name))
                 return "Item";
-            if (Alpha.IndexOf(name[0]) == -1)
+            if (!char.IsLetter(name[0]))
                 return string.Format("Item{0}", name);
             return name;
         }
