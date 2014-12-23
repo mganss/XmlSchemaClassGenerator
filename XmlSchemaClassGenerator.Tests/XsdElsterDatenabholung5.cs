9d43106f1b57e0f465fe518e8f0f2ac351e4838c
Mark Junker <m.junker@fubar-dev.de>
2014-12-23 13:16:47 +0100

    Test optimization, thanks to TestPriority

--- XmlSchemaClassGenerator.Tests/XsdElsterDatenabholung5.cs
+++ XmlSchemaClassGenerator.Tests/XsdElsterDatenabholung5.cs
@@ -7,50 +7,61 @@ using Xunit;
 
 namespace XmlSchemaClassGenerator.Tests
 {
+    [PrioritizedFixture]
     public class XsdElsterDatenabholung5
     {
-        private void GenerateOutput(string outputPath)
+        static XsdElsterDatenabholung5()
         {
-            var gen = new Generator
+            // Ensure that the output directories are empty.
+            Directory.Delete(GetOutputPath(string.Empty), true);
+        }
+
+        [Fact, TestPriority(1)]
+        public void CanGenerateClasses()
+        {
+            Assert.DoesNotThrow(() =>
             {
-                EnableDataBinding = true,
-                IntegerDataType = typeof(int),
-                UseXElementForAny = true,
-                CollectionType = typeof(IList<>),
-                CollectionImplementationType = typeof(List<>),
-                GenerateDesignerCategoryAttribute = false,
-                GenerateNullables = true,
-                GenerateSerializableAttribute = false,
-                OutputFolder = outputPath,
-                NamingScheme = NamingScheme.Direct,
-                DataAnnotationMode = DataAnnotationMode.None,
-                EmitOrder = true,
-                NamespaceProvider = new NamespaceProvider
+                var outputPath = GetOutputPath("CanGenerateClasses");
+                var gen = new Generator
                 {
-                    GenerateNamespace = key =>
+                    EnableDataBinding = true,
+                    IntegerDataType = typeof(int),
+                    UseXElementForAny = true,
+                    CollectionType = typeof(IList<>),
+                    CollectionImplementationType = typeof(List<>),
+                    GenerateDesignerCategoryAttribute = false,
+                    GenerateNullables = true,
+                    GenerateSerializableAttribute = false,
+                    OutputFolder = outputPath,
+                    NamingScheme = NamingScheme.Direct,
+                    DataAnnotationMode = DataAnnotationMode.None,
+                    EmitOrder = true,
+                    NamespaceProvider = new NamespaceProvider
                     {
-                        switch (Path.GetFileName(key.Source.LocalPath))
+                        GenerateNamespace = key =>
                         {
-                            case "th000008_extern.xsd":
-                            case "ndh000010_extern.xsd":
-                            case "headerbasis000002.xsd":
-                                return "Elster.Basis";
-                            case "datenabholung_5.xsd":
-                            case "elster0810_datenabholung_5.xsd":
-                                switch (key.XmlSchemaNamespace)
-                                {
-                                    case "http://www.elster.de/2002/XMLSchema":
-                                        return "Elster.Datenabholung5";
-                                    default:
-                                        throw new NotSupportedException(string.Format("Namespace {0} for schema {1}", key.XmlSchemaNamespace, key.Source));
-                                }
-                            default:
-                                throw new NotSupportedException(string.Format("Namespace {0} for schema {1}", key.XmlSchemaNamespace, key.Source));
+                            switch (Path.GetFileName(key.Source.LocalPath))
+                            {
+                                case "th000008_extern.xsd":
+                                case "ndh000010_extern.xsd":
+                                case "headerbasis000002.xsd":
+                                    return "Elster.Basis";
+                                case "datenabholung_5.xsd":
+                                case "elster0810_datenabholung_5.xsd":
+                                    switch (key.XmlSchemaNamespace)
+                                    {
+                                        case "http://www.elster.de/2002/XMLSchema":
+                                            return "Elster.Datenabholung5";
+                                        default:
+                                            throw new NotSupportedException(string.Format("Namespace {0} for schema {1}", key.XmlSchemaNamespace, key.Source));
+                                    }
+                                default:
+                                    throw new NotSupportedException(string.Format("Namespace {0} for schema {1}", key.XmlSchemaNamespace, key.Source));
+                            }
                         }
                     }
-                }
-            };
-            var xsdFiles = new[]
+                };
+                var xsdFiles = new[]
                 {
                     "headerbasis000002.xsd",
                     "ndh000010_extern.xsd",
@@ -58,27 +69,15 @@ namespace XmlSchemaClassGenerator.Tests
                     "datenabholung_5.xsd",
                     "elster0810_datenabholung_5.xsd",
                 }.Select(x => Path.Combine(InputPath, x)).ToList();
-            gen.Generate(xsdFiles);
-        }
-
-        [Fact]
-        public void CanGenerateClasses()
-        {
-            Assert.DoesNotThrow(() =>
-            {
-                GenerateOutput(GetOutputPath("CanGenerateClasses"));
+                gen.Generate(xsdFiles);
             });
         }
 
-        [Fact]
+        [Fact, TestPriority(2)]
         public void CanCompileClasses()
         {
+            var inputPath = GetOutputPath("CanGenerateClasses");
             var outputPath = GetOutputPath("CanCompileClasses");
-            Assert.DoesNotThrow(() =>
-            {
-                new DirectoryInfo(outputPath).GetFiles("*.cs").ToList().ForEach(x => x.Delete());
-                GenerateOutput(outputPath);
-            });
             var provider = CodeDomProvider.CreateProvider("CSharp");
             var assemblies = new[]
             {
@@ -90,19 +89,19 @@ namespace XmlSchemaClassGenerator.Tests
                 "System.ServiceModel.dll",
             };
             var outputName = Path.Combine(outputPath, "Elster.Test.dll");
-            var fileNames = new DirectoryInfo(outputPath).GetFiles("*.cs").Select(x => x.FullName).ToArray();
+            var fileNames = new DirectoryInfo(inputPath).GetFiles("*.cs").Select(x => x.FullName).ToArray();
             var results = provider.CompileAssemblyFromFile(new CompilerParameters(assemblies, outputName), fileNames);
             Assert.Equal(0, results.Errors.Count);
             Assert.DoesNotThrow(() => results.CompiledAssembly.GetType("Elster.Datenabholung5.Elster", true));
             Assert.DoesNotThrow(() => results.CompiledAssembly.GetType("Elster.Basis.TransferHeaderCType", true));
         }
 
-        string InputPath
+        private static string InputPath
         {
             get { return Path.Combine(Directory.GetCurrentDirectory(), "xsd", "elster-xml-datenabholung5"); }
         }
 
-        private string GetOutputPath(string testCaseId)
+        private static string GetOutputPath(string testCaseId)
         {
             var result = Path.Combine(Directory.GetCurrentDirectory(), "output", "elster-xml-datenabholung5", testCaseId);
             Directory.CreateDirectory(result);
