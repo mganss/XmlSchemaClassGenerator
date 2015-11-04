57869efcf55b4d7f9918a656a5edccf615816a7e
Michael Ganss <michael.ganss@updatestar.com>
2015-11-04 16:48:22 +0100

    Build through AppVeyor

--- XmlSchemaClassGenerator/Properties/AssemblyInfo.cs
+++ XmlSchemaClassGenerator/Properties/AssemblyInfo.cs
@@ -22,14 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("de5a48df-c5c0-4efc-9516-264d4e76d2a9")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.*")]
+// assembly version information will be reset by AppVeyor
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
