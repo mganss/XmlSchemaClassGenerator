f33c5868e12a9d96a62720a8078631c05aed7672
Michael Ganss <michael.ganss@updatestar.com>
2017-06-06 16:12:26 +0200

    Target netstandard2.0 and net45

--- XmlSchemaClassGenerator/Properties/AssemblyInfo.cs
+++ XmlSchemaClassGenerator/Properties/AssemblyInfo.cs
@@ -1,18 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("XmlSchemaClassGenerator")]
-[assembly: AssemblyDescription("A console program and library to generate XmlSerializer compatible C# classes from XML Schema files.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Michael Ganss")]
-[assembly: AssemblyProduct("XmlSchemaClassGenerator")]
-[assembly: AssemblyCopyright("Copyright © 2013-2015 Michael Ganss")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+#if !NETSTANDARD
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -22,7 +11,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("de5a48df-c5c0-4efc-9516-264d4e76d2a9")]
 
-// assembly version information will be reset by AppVeyor
+#endif
+
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
