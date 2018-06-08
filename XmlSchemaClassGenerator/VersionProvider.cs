ebf1f1a902bdbbbb29ca119aa080d7367d12cff5
Taylor Southwick <twsouthwick@outlook.com>
2018-06-07 18:47:26 -0700

    Add version provider to allow for testing of output

---
+++ XmlSchemaClassGenerator/VersionProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace XmlSchemaClassGenerator
+{
+    [DebuggerDisplay("{Title} - {Version}")]
+    public class VersionProvider
+    {
+        public VersionProvider(string title, string version)
+        {
+            Title = title;
+            Version = version;
+        }
+
+        public string Title { get; }
+
+        public string Version { get; }
+
+        public static VersionProvider CreateFromAssembly()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var title = executingAssembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+            var version = executingAssembly.GetName().Version.ToString();
+
+            return new VersionProvider(title, version);
+        }
+    }
+}
