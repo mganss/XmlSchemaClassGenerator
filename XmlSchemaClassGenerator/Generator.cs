cadf28c58f7ecc2014943711bf637893b5d30aa8
Philemon Eichin <phileicht@gmx.de>
2023-06-18 21:45:14 +0200

    Log line numbers for `XmlSchemaException`

--- XmlSchemaClassGenerator/Generator.cs
+++ XmlSchemaClassGenerator/Generator.cs
@@ -335,7 +335,14 @@ namespace XmlSchemaClassGenerator
                 var ex = e.Exception as Exception;
                 while (ex != null)
                 {
-                    Log?.Invoke(ex.Message);
+                    if(ex is XmlSchemaException xmlException)
+                    {
+                        Log?.Invoke(FormatXmlSchemaExeption(xmlException));
+                    }
+                    else
+                    {
+                        Log?.Invoke(ex.Message);
+                    }
                     ex = ex.InnerException;
                 }
             };
@@ -377,6 +384,28 @@ namespace XmlSchemaClassGenerator
 
                 writer.Write(ns);
             }
+        }
+
+        private string FormatXmlSchemaExeption(XmlSchemaException e)
+        {
+            StringBuilder sb = new StringBuilder(e.Message);
+
+            if(!string.IsNullOrEmpty(e.SourceUri))
+            {
+                sb.AppendFormat(" at {0}:{1}:{2}",
+                    e.SourceUri,
+                    e.LineNumber, 
+                    e.LinePosition);
+            }
+            else if(e.SourceSchemaObject != null)
+            {
+                sb.AppendFormat(" at {0}:{1}:{2}",
+                    e.SourceSchemaObject.SourceUri,
+                    e.SourceSchemaObject.LineNumber,
+                    e.SourceSchemaObject.LinePosition);
+            }
+
+            return sb.ToString();
         }
     }
 }
